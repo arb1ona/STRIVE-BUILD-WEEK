be22cad7597f22bace7784f84a6fa3301b9f70ce
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-06-19 18:22:01 +0200

    Today News Added

---
+++ BUILD WEEK 2 - LinkedIn/linkedin/src/components/SideProfile.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import{Button,Row,Col} from 'react-bootstrap'
+import './SideProfile.css'
+import Viewed from './Viewed'
+
+class SideProfile extends Component {
+    render() { 
+        return ( 
+            <div className='wrapper'>
+            <Row>
+                <Col>
+                <span>Edit public profile & URL</span>
+                </Col>
+                <Col>
+                <i className="fa fa-edit "></i>
+                </Col>
+            </Row>
+            <div className='discount container'>
+            <div className='d-flex flex-row-reverse'>
+                <i className="fa fa-ellipsis-h mx-2"></i>
+                <span>Ad</span>
+            </div>
+            <div className="d-flex flex-column">
+            <span>Quadri,invest in your future with this exclusive offer</span>
+            <img className='logo mx-5' alt=''src="https://image.shutterstock.com/image-vector/profile-blank-icon-empty-photo-260nw-535853269.jpg"></img>
+            </div>
+            <p>Enjoy 50% off 2 months of Linkedin Premium!</p>
+            <div className="d-flex justify-content-center">
+            <Button variant="outline-secondary">Get 50% off today</Button>
+            </div>
+            </div>
+                <Viewed/>
+            </div>
+     
+        
+         );
+    }
+}
+ 
+export default SideProfile;
