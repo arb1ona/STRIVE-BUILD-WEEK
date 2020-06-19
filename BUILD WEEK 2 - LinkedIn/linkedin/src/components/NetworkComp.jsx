e91331730fbe5e48c373dd981da5907a659562fa
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-06-19 18:19:53 +0200

    Network Addded

---
+++ BUILD WEEK 2 - LinkedIn/linkedin/src/components/NetworkComp.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card, Button, Row, CardDeck, Container } from 'react-bootstrap'
+import './Networkc.css'
+const NetworkComp = (props) => {
+    return (
+        <>
+            <Card id="card" className=' '>
+                <Card.Img src={props.data.image} id="card" />
+                <Card.Body>
+                    <Card.Title>
+                        <span>{props.data.name}</span>
+                        <span>{props.data.surname}</span>
+                    </Card.Title>
+
+                    <Card.Text>
+                        {props.data.title}
+                     Mutual connection
+                </Card.Text>
+                </Card.Body>
+
+                <Card.Footer>
+                    <Button variant="outline-primary" size='lg' className='bg-lg'>Connect</Button>
+                </Card.Footer>
+
+                <Card.Text><small className="lead mt-2">5 mutual connection</small></Card.Text>
+                <Link to={'Profile' + props.data.username}><Button variant="outline-primary" size='lg' className='bg-lg'>Connect</Button></Link>
+            </Card >
+        </>
+    )
+}
+
+
+export default NetworkComp
---
+++ BUILD WEEK 2 - LinkedIn/linkedin/src/components/NetworkP.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Profile.css'
+import NetworkComp from './NetworkComp'
+import NewsFeedWelcome from './NewsFeedWelcome'
+
+
+class NetworkP extends React.Component {
+
+    state = {
+        persons: []
+    }
+
+    getNetwork = async () => {
+        let response = await fetch("https://striveschool.herokuapp.com/api/profile/", {
+            method: "GET",
+            headers: {
+                Authorization: "Basic dXNlcjU6OVVWbVFEYVNQZ3hTdUFkcw== ",
+            }
+        });
+
+        let persons = await response.json()
+        console.log(persons)
+        this.setState({ persons: persons })
+        console.log(this.state.persons)
+    }
+
+
+
+    componentDidMount = async () => {
+        await this.getNetwork()
+    }
+
+    render() {
+
+        return (
+            <>
+                <div>No pending Invitations</div>
+                <div className="container mt-4  py-5">
+                    <div className="row text-center ">
+                        <div className="col-lg-2">
+                            <NewsFeedWelcome />
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="row">
+
+                                {this.state.persons.map(person => {
+
+                                    return (
+                                        <div className="col-lg-3 mb-2">
+                                            <NetworkComp key={person.id} data={person} />
+                                        </div>
+
+
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+
+}
+
+
+
+export default NetworkP
